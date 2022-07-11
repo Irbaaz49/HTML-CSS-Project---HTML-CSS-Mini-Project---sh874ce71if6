d6694fc933c423ed1db6dcf999980ba1cb73084d
irbaaz <mwater196@gmail.com>
2022-07-11 23:14:26 +0530

    ProjectCompleted

--- App.js
+++ App.js
@@ -1,1 +1,25 @@
+const counter = document.querySelectorAll('.counter');
+
+
+console.log(counter)
+counter.forEach((element)=>{
+console.log(element);
+element.innerHTML = '0';
+
+const updateCounter =()=>{
+const targetCount = +element.getAttribute('data-target');
+const startingCount = Number(element.innerHTML); 
+const incr = targetCount/10;
+if(startingCount<targetCount){
+    element.innerHTML = startingCount + incr;
+setTimeout(updateCounter,500);
+}else{
+    element.innerHTML = targetCount;
+}
+
+}
+
+
+updateCounter();
+})
 
